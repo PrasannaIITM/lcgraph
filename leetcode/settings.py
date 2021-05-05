33770c4456dc003274c29963a1477653d4289a07
prasanna <prasanna.bartakke@gmail.com>
2021-05-05 13:54:38 +0530

    debug true commit

--- leetcode/settings.py
+++ leetcode/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')xws+m$uo5-c1kr!e@t(94u11=@543hi=xpfrtm@ge3tdg(k9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'lccontestgraph.herokuapp.com']
 
