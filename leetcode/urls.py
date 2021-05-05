38e890c66531758fdb6bb6acc00e9c2c586b2f6e
prasanna <prasanna.bartakke@gmail.com>
2021-05-05 13:15:13 +0530

    added static root commit

--- leetcode/urls.py
+++ leetcode/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('visualize/', new),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
 ]
